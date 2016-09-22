fc32ff32b9ad8e59b42542c288944c4fa32b8fd9
Robert Wells <rwells@modelab.is>
2016-09-22 15:45:17 -0400

    change debug to info

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
 
         // This is called after parsing the book, before generating output and pages.
         "init": function() {
-            this.log.debug.ln('Running init scripts...');
+            this.log.info.ln('Running init scripts...');
             scriptdir = this.options.pluginsConfig.scriptDir || scriptdir;
             if (this.options.pluginsConfig && this.options.pluginsConfig.scripts.init) {
                 var initscripts = this.options.pluginsConfig.scripts.init;
@@ -20,7 +20,7 @@ module.exports = {
 
         // This is called before running the templating engine on the page.
         "page:before": function(page) {
-            this.log.debug.ln('Running page:before scripts...' , page);
+            this.log.info.ln('Running page:before scripts...' , page);
             if (this.options.pluginsConfig && this.options.pluginsConfig.scripts.pageBefore) {
                 var pagebeforescripts = this.options.pluginsConfig.scripts.pageBefore;
                 for (var i = 0; i < pagebeforescripts.length; i++) {
@@ -32,7 +32,7 @@ module.exports = {
 
         // This is called before outputting and indexing the page.
         "page": function(page) {
-            this.log.debug.ln('Running page scripts...' , page);
+            this.log.info.ln('Running page scripts...' , page);
             if (this.options.pluginsConfig && this.options.pluginsConfig.scripts.page) {
                 var pagescripts = this.options.pluginsConfig.scripts.page;
                 for (var i = 0; i < pagescripts.length; i++) {
@@ -44,7 +44,7 @@ module.exports = {
 
         // This is called after generating the pages, before copying assets, cover, ...
         "finish:before": function() {
-            this.log.debug.ln('Running finish:before scripts...');
+            this.log.info.ln('Running finish:before scripts...');
             if (this.options.pluginsConfig && this.options.pluginsConfig.scripts.finishBefore) {
                 var finishbeforescripts = this.options.pluginsConfig.scripts.finishBefore;
                 for (var i = 0; i < finishbeforescripts.length; i++) {
@@ -56,7 +56,7 @@ module.exports = {
 
         // This is called after everything else.
         "finish": function() {
-            this.log.debug.ln('Running finish scripts...');
+            this.log.info.ln('Running finish scripts...');
             if (this.options.pluginsConfig && this.options.pluginsConfig.scripts.finish) {
                 var finishscripts = this.options.pluginsConfig.scripts.finish;
                 for (var i = 0; i < finishscripts.length; i++) {
